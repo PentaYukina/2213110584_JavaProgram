7ff92bbf205e10e998a93c315ddce933205c7bd8
PentaYukina <jarahaama@gmail.com>
2022-12-23 15:12:44 +0700

    Exercise[22-12-2022]

---
+++ javaProgram/src/CheckStockProduct.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CheckStockProduct {
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("How many product list in stock : ");
+		int product = scan.nextInt();
+		Product[] pd = new Product[product];
+		System.out.println();
+
+		for (int i = 0; i < pd.length; i++) {
+			pd[i] = new Product();
+			System.out.print("Input product Id : ");
+			pd[i].setId(scan.next());
+			System.out.print("Input product Unit : ");
+			pd[i].setUnit(scan.nextInt());
+			System.out.println();
+		}
+
+		System.out.println("--------------------------------------");
+		System.out.println("LIST of product in 'LOW' status.");
+		System.out.println("--------------------------------------");
+		for (Product _pd : pd) {
+			if(_pd.productStatus(_pd.getUnit()).equalsIgnoreCase("LOW")) 
+			System.out.println(">> " + _pd.getId() + " has " + _pd.getUnit() + " units");
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("LIST of product in 'NORMAL' status.");
+		System.out.println("--------------------------------------");
+		for (Product _pd : pd) {
+			if(_pd.productStatus(_pd.getUnit()).equalsIgnoreCase("NORMAL")) 
+			System.out.println(">> " + _pd.getId() + " has " + _pd.getUnit() + " units");
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("LIST of product in 'HIGH' status.");
+		System.out.println("--------------------------------------");
+		for (Product _pd : pd) {
+			if(_pd.productStatus(_pd.getUnit()).equalsIgnoreCase("HIGH")) 
+			System.out.println(">> " + _pd.getId() + " has " + _pd.getUnit() + " units");
+		}
+	}
+}
---
+++ javaProgram/src/StockProduct.java
@@ -0,0 +1,30 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class StockProduct {
+	static Scanner scan = new Scanner(System.in);
+	static Product[] pd = new Product[4];
+	
+	public static void main(String[] args) {
+		DecimalFormat frm = new DecimalFormat("#,###.00");
+		for (int i = 0; i < pd.length; i++) {
+			pd[i] = new Product();
+			System.out.print("Input product Id : ");
+			pd[i].setId(scan.next());
+			System.out.print("Input product Unit : ");
+			pd[i].setUnit(scan.nextInt());
+			System.out.print("Input product Price : ");
+			pd[i].setPrice(scan.nextDouble());
+			System.out.println();
+		}
+		System.out.println("-------------------------------------------");
+		double total = 0;
+		for (Product _pd : pd) {
+			System.out.println("Product ID : " + _pd.getId() + ", Total price = " + frm.format(_pd.calculate())+" baht.");
+			total += _pd.calculate();
+		}
+		System.out.println("-------------------------------------------");
+		System.out.println("Total price of all products is "+frm.format(total)+" baht.");
+	}
+
+}
